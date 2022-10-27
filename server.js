c5ff34e8a0ec2604bb83ad25df8aab2899d2a68d
Maxi <maxi.cespedes@wetransfer.com>
2022-10-27 13:29:34 -0300

    Vmoas??

---
+++ server.js
@@ -0,0 +1,34 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({
+  port: 3099,
+});
+
+const state = {
+  users: {},
+  buttons: {},
+};
+
+wss.on("connection", function connection(ws) {
+  state.users[ws] = { id: '0' }; 
+
+  // Message from the client 
+  ws.on("message", function message(data) {
+    console.log(data);
+    const message = JSON.parse(data);
+    
+    if (message.type === 'ASSIGN_USER_DATA') {
+     // HERE si 
+    }
+  });
+
+
+  // HERE you can mock some message to the client
+  setInterval(() => {
+    ws.send(mess({ type: 'PONG'}));
+  }, 5000);
+
+  ws.send(mess({ type: "INITIAL_STATE", users: sendInitialState() }));
+});
+
+console.log("Listening on port 3099");
